691ec5f4bdd66dd9d34493ab62b75b7f5adb8beb
Jordi <Jordony36@gmail.com>
2025-10-29 15:25:29 -0400

    notification commit

--- app/(tabs)/notifications.tsx
+++ app/(tabs)/notifications.tsx
@@ -1,10 +1,29 @@
-import React from "react";
-import { Text } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
 const NotificationsPage = () =>{
     return(
-    <Text>amongus</Text>
-    )
-}
+        <View style={styles.container}>
+            <Text style={styles.text}>discord</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#1e2124",
+        justifyContent: "flex-start",
+        alignItems: "flex-start",
+        paddingTop: 40,
+        paddingLeft: 16,
+    },
+    text: {
+        color: "#ffffff",
+        fontSize: 24,
+        fontWeight: "bold",
+
+    },
+    
+});
 
 export default NotificationsPage;
